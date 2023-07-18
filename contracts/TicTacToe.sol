3f28a95f63dbc0657138be58e44b8c6e48dc5feb
Miguer-dev <miguerdev@gmail.com>
2023-07-17 23:17:18 -0400

    Updated contract "TicTacToe"

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.12 <9.0.0; 
+
+contract TicTacToe {
+    
+    struct Match{
+        address player1;
+        address player2;             
+        Turn nextTurn;
+        address winner;        
+        address [9] board;
+    }   
+
+    struct Turn{
+        uint number; 
+        address player;  
+    }
+
+    Match[] public matches;
+
+    event newMatch(   
+        uint idMatch,
+        address player1, 
+        address player2 
+    ); 
+
+    event newPlay(  
+        uint idMatch,
+        uint cell,
+        address player
+    );
+
+    event winnerMatch(  
+        uint idMatch,        
+        address player
+    );
+
+    modifier matchExist(uint idMatch){
+        require(matches.length > idMatch, "The match does not exist");         
+        _;
+    }
+
+    modifier playerOfTheMatch(uint idMatch){
+        require(matches[idMatch].player1 == _msgSender() || matches[idMatch].player2 == _msgSender(), "You are not a player of this match");         
+        _;
+    }
+
+    modifier matchInProgress(uint idMatch){
+        require(matches[idMatch].nextTurn.number < 10 && matches[idMatch].winner == address(0), "The match has already ended");         
+        _;
+    }
+
+     modifier myTurn(uint idMatch){
+        require(matches[idMatch].nextTurn.player == _msgSender() || matches[idMatch].nextTurn.player == address(0), "It's not your turn");         
+        _;
+    }
+
+    modifier wrongPlay(uint cell){
+        require(cell >= 1 && cell <= 9, "Wrong move, the possible plays are from 1 to 9");         
+        _;
+    }
+  
+    modifier allowedPlay(uint idMatch, uint cell){
+        require(matches[idMatch].board[cell-1] == address(0), "That move was already made");         
+        _;
+    }
+   
+
+    function initMatch(address player1, address player2) public returns (uint){
+
+        address[9] memory newBoard = [address(0),address(0),address(0),address(0),address(0),address(0),address(0),address(0),address(0)];  
+        Match memory game = Match(player1,player2,Turn(1,address(0)),address(0),newBoard);
+        matches.push(game);
+        emit newMatch(matches.length - 1, player1, player2);
+
+        return matches.length - 1;
+    }
+
+    function play(uint idMatch, uint cell) matchExist(idMatch) playerOfTheMatch(idMatch) matchInProgress(idMatch) myTurn(idMatch) wrongPlay(cell) allowedPlay(idMatch,cell)  public{
+ 
+        matches[idMatch].board[cell-1] = _msgSender();
+        matches[idMatch].nextTurn = Turn(matches[idMatch].nextTurn.number + 1, _msgSender() == matches[idMatch].player1 ? matches[idMatch].player2 : matches[idMatch].player1);
+        emit newPlay(idMatch, cell, _msgSender());
+
+        if ( _getWinner(idMatch) != address(0) ){
+            matches[idMatch].winner = _msgSender();
+            emit winnerMatch(idMatch, _msgSender());
+        }  
+    }
+
+    function _getWinner(uint idMatch) internal view returns (address) {
+
+        address result = address(0);
+
+        if((matches[idMatch].board[0] == matches[idMatch].board[0+1] && 
+            matches[idMatch].board[0] == matches[idMatch].board[0+2])   ||
+           (matches[idMatch].board[0] == matches[idMatch].board[0+3] &&
+            matches[idMatch].board[0] == matches[idMatch].board[0+6])   || 
+           (matches[idMatch].board[0] == matches[idMatch].board[0+4] && 
+            matches[idMatch].board[0] == matches[idMatch].board[0+8]))
+        {
+            result = matches[idMatch].board[0];    
+        }
+        else if(matches[idMatch].board[1] == matches[idMatch].board[1+3] && 
+                matches[idMatch].board[1] == matches[idMatch].board[1+6])                       
+        {
+            result = matches[idMatch].board[1];   
+        }
+        else if((matches[idMatch].board[2] == matches[idMatch].board[2+2] && 
+                 matches[idMatch].board[2] == matches[idMatch].board[2+4])  ||
+                (matches[idMatch].board[2] == matches[idMatch].board[2+3] && 
+                 matches[idMatch].board[2] == matches[idMatch].board[2+6]))                       
+        {
+            result = matches[idMatch].board[2];   
+        }
+        else if(matches[idMatch].board[3] == matches[idMatch].board[3+1] && 
+                matches[idMatch].board[3] == matches[idMatch].board[3+2])                       
+        {
+            result = matches[idMatch].board[3];   
+        }
+        else if(matches[idMatch].board[6] == matches[idMatch].board[6+1] && 
+                matches[idMatch].board[6] == matches[idMatch].board[6+2])                       
+        {
+            result = matches[idMatch].board[6];   
+        }
+
+        return result;
+    }
+ 
+    function _msgSender() internal view returns (address) {
+        return msg.sender;
+    }    
+}
